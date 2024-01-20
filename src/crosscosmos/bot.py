34cc6cea7d3b43af3f4a27a9367b5aab0819fef9
Nick LaFarge <nick.lafarge@jhuapl.edu>
2024-01-19 22:15:01 -0500

    Bot working (but with repeated words)

--- src/crosscosmos/bot.py
+++ src/crosscosmos/bot.py
@@ -1,3 +1,6 @@
+# Standard
+from enum import Enum
+
 # Third-party
 import logging
 import pygtrie
@@ -7,6 +10,37 @@ import crosscosmos as xc
 from crosscosmos.grid import CellStatus
 
 logger = logging.getLogger(__name__)
+
+
+class GridStatus(Enum):
+    COMPLETE = 1
+    INCOMPLETE = 2
+    INVALID = 3
+
+
+class MoveDirection(Enum):
+    FORWARD = 1
+    BACK = 2
+
+
+class LetterStatus(Enum):
+    VALID = 1
+    INVALID = 2
+
+
+def get_previous_cell_index(i: int, j: int):
+    pass
+
+
+def validate_grid_letter_sequence(grid_trie: pygtrie, letter_sequence: str, is_end: bool):
+    node_in_trie = grid_trie.has_node(letter_sequence)
+    if is_end and node_in_trie == pygtrie.Trie.HAS_VALUE:
+        return True
+    elif not is_end and node_in_trie == pygtrie.Trie.HAS_SUBTRIE:
+        return True
+    else:
+        return False
+
 
 if __name__ == '__main__':
     lc = xc.corpus.Corpus.from_test()
@@ -22,80 +56,102 @@ if __name__ == '__main__':
     #     for j in range(grid.col_count):
     i = 0
     j = 0
-    grid_full = False
+    grid_status = GridStatus.INCOMPLETE
     h_word_counter = 0
-    while not grid_full:
+
+    # Get the next value that exists
+    while grid_status == grid_status.INCOMPLETE:
         c = grid[i, j]
 
-        # Get the next value that exists
-        move_back = False
-        val_is_valid = False
+        # Initialize variables
+        move_dir = MoveDirection.FORWARD
+        letter_status = LetterStatus.INVALID
 
         # Continue on if this square is black
         if c.status == CellStatus.BLACK:
-            val_is_valid = True
+            letter_status = LetterStatus.VALID
 
-        # Continue on if this is locked, move back if the subtrie or word isn't valid
+        # When a locked square is encountered, do the following:
+        #    - Locked squared are always treated as valid
+        #    - Move back if adding the locked square's value creates an invalid subtrie
+        #    - Move back if the locked square is at a barrier and a word isn't formed
         if c.status == CellStatus.LOCKED:
-            val_is_valid = True
 
+            # A locked square is automatically considered "Valid"
+            letter_status = LetterStatus.VALID
+
+            # Get the cross word up to this point
             word_to_xy = grid.get_h_word_up_to(c.x, c.y)
+
+            # See if the word up to now is valid. If not, move back
             t_has_node = trie.has_node(word_to_xy)
             if not t_has_node:
-                move_back = True
-            elif c.is_h_end and t_has_node == pygtrie.Trie.HAS_SUBTRIE:
-                move_back = True
+                move_dir = MoveDirection.BACK
 
-        # Look for a valid letter
-        while not val_is_valid:
+            # If we've reached a horizontal barrier and don't have a word, then move back
+            if c.is_h_end and t_has_node == pygtrie.Trie.HAS_SUBTRIE:
+                move_dir = MoveDirection.BACK
 
+        # Choose the next letter by proceeding through the grid entry's letter list and selecting the
+        # first letter than yields a valid subtrie
+        while letter_status != LetterStatus.VALID:
+
+            # If there are no more letters, then no word exists, and we need to move back
             if len(grid[i, j].queue) == 0:
-                move_back = True
+                move_dir = MoveDirection.BACK
                 break
 
-            # Update actual value for c within grid
+            # Set the value in the grid with the next letter in the queue
             grid[i, j].value = grid[i, j].queue.pop()
             grid[i, j].status = CellStatus.SET
 
+            # Check if the horizontal letter sequence is valid
             h_word_to_xy = grid.get_h_word_up_to(c.x, c.y)
+            horizontally_valid = validate_grid_letter_sequence(trie, h_word_to_xy, c.is_h_end)
+
+            # Check if the vertical letter sequence is valid
             v_word_to_xy = grid.get_v_word_up_to(c.x, c.y)
+            vertically_valid = validate_grid_letter_sequence(trie, v_word_to_xy, c.is_v_end)
 
-            trie_has_h_node = trie.has_node(h_word_to_xy)
-            trie_has_v_node = trie.has_node(v_word_to_xy)
+            # The selected letter is only accepted if it is valid in both vertical and horizontal directions
+            if horizontally_valid and vertically_valid:
+                letter_status = letter_status.VALID
 
-            # Must be valid in both horizontal and vertical directions
-            if c.is_h_end and trie_has_h_node == pygtrie.Trie.HAS_VALUE:
-                val_is_valid = True
-            elif not c.is_h_end and trie_has_h_node == pygtrie.Trie.HAS_SUBTRIE:
-                val_is_valid = True
-
-            if c.is_v_end and trie_has_v_node == pygtrie.Trie.HAS_VALUE:
-                val_is_valid = True
-            elif not c.is_v_end and trie_has_v_node == pygtrie.Trie.HAS_SUBTRIE:
-                val_is_valid = True
-
-        # Move back one index if instructed
+        # For debugging
         grid.print()
-        if move_back:
-            c.reset_cell()
-            if j > 0:
-                j -= 1
-            elif j == 0 and i > 0:
-                j = grid.col_count - 1
-                i -= 1
-            else:
-                raise EOFError("No Solutions Found for Grid")
-        else:
-            if j < grid.col_count - 1:
-                j += 1
-            elif j == grid.col_count - 1 and i < grid.row_count - 1:
-                j = 0
-                i += 1
-            else:
-                grid_full = True  # COMPLETE!
 
-        # Move right until we can't
-        # while hj < grid.col_count and c.status != CellStatus.BLACK:
-        #
-        #     Go to the next cell
-        # j += 1
+        # Save for readability
+        on_left_column = j == 0
+        on_right_column = j == (grid.col_count - 1)
+        on_top_row = i == 0
+        on_bottom_row = i == (grid.row_count - 1)
+
+        # If an invalid configuration is encountered, move back to the previous cell and reset the current one
+
+        match move_dir:
+            case MoveDirection.FORWARD:
+                if on_bottom_row and on_right_column:  # COMPLETE!
+                    grid_status = GridStatus.COMPLETE
+                if j < grid.col_count - 1:
+                    j += 1
+                elif j == grid.col_count - 1 and i < grid.row_count - 1:
+                    j = 0
+                    i += 1
+
+            case MoveDirection.BACK:
+                c.reset_cell()
+
+                if on_left_column and on_top_row:  # We aren't square one an out of options!
+                    grid_status = GridStatus.INVALID
+                elif on_left_column:  # Move one row up to the right-most column
+                    j = grid.col_count - 1
+                    i -= 1
+                else:  # Move one square to the left
+                    j -= 1
+
+    match grid_status:
+        case GridStatus.COMPLETE:
+            print("Grid complete!")
+        case GridStatus.INVALID:
+            print("No valid solution found for grid")
+    grid.print()
