7701a390f6fc37f6b0702160374d0ed0c99eaee6
nlafarge <nick.lafarge@gmail.com>
2024-02-21 20:22:10 -0500

    Fix set color load bug

--- scripts/famous_last_words/famous_last_words.py
+++ scripts/famous_last_words/famous_last_words.py
@@ -5,7 +5,7 @@ import crosscosmos as xc
 grids_path = Path(__file__).parent / 'grids'
 
 # Create grid backend
-grid_path = Path(grids_path / "famous_last_words.json")
+grid_path = Path(grids_path / "famous_last_words2.json")
 xc_grid = xc.grid.Grid.load(grid_path)
 xc_grid.corpus = xc.corpus.Corpus.from_test()
 
