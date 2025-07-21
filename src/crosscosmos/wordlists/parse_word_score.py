331f4d70edb3e894c6339c99e058da2cb131dde4
Nick LaFarge <nick.lafarge@jhuapl.edu>
2025-07-21 19:45:18 -0400

    Added database for "spread the word" list

--- src/crosscosmos/wordlists/parse_word_score.py
+++ src/crosscosmos/wordlists/parse_word_score.py
@@ -6,27 +6,25 @@ Source (Broda):
 Source (Diehl Trimmed List):
     https://www.facebook.com/groups/1515117638602016/files
 
+Source (Spread the word list):
+    https://www.spreadthewordlist.com/wordlist
+
 """
 
-# Standard library imports
-import csv
 import logging
 import pathlib
 
-# Third-party imports
-
-# Local imports
 import crosscosmos as xc
 
 logger = logging.getLogger(__name__)
 
 
-def parse_word_score(word_score_path: pathlib.Path, word_model, delimiter: str):
+def parse_word_score(word_score_path: pathlib.Path, word_model, delimiter: str, score_multiplier: int = 1):
     i = 0
     for row in xc.wordlists.parsing_utils.read_csv_generator(word_score_path, delimiter):
-        if i % 100 == 0:
+        if i % 1000 == 0:
             print(i)
         word, score = row
 
-        word_model(word=word, score=score)
+        word_model(word=word, score=int(int(score) * score_multiplier))
         i += 1
